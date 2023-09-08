638d31f68419f8ec5d350a703de6a549d63a65f3
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-09-08 19:45:27 -0400

    update cards fix

---
+++ src/api/ShowMoviesShows.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import ShowCards from "../components/ShowCards";
+
+function ShowMoviesShows({ category }) {
+  const API_KEY = "c566f55b172728d7f84abac7efdfa535";
+
+  const [moviePosters, setMoviePosters] = useState([]);
+  const [movieTitles, setMovieTitles] = useState([]);
+
+  useEffect(() => {
+    async function fetchMoviePosters() {
+      try {
+        const res = await fetch(
+          `https://api.themoviedb.org/3/movie/${category}?api_key=${API_KEY}`
+        );
+
+        if (res.ok) {
+          const data = await res.json();
+          const posters = data.results.map((movie) => movie.backdrop_path);
+          const titles = data.results.map((movie) => movie.title);
+          setMoviePosters(posters);
+          setMovieTitles(titles);
+        } else {
+          console.error(`Error fetching ${category} movies`);
+        }
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+    }
+
+    fetchMoviePosters();
+  }, [category]);
+
+  return <ShowCards posters={moviePosters} titles={movieTitles} />;
+}
+
+export default ShowMoviesShows;
