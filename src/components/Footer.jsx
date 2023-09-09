d040cd2b8468f6b97f95a7b2e529c330514a2076
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-09-09 01:17:44 -0400

    update cards 3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
       <ul className="footer2">
         <li>Terms of Use</li>
         <li>Privacy Policy</li>
-        <li>Willing to put in the Work</li>
+        <li>I'll Turn Design To Code</li>
         <li>2023 | Hulu By Axel</li>
       </ul>
     </footer>
